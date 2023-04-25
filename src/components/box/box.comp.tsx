8a19b2390a33c91050bc0cee70f5b399c63b032e
Alex McGovern <alex_mcgovernsmith@icloud.com>
2023-04-25 18:13:11 +0100

    fix(css): don't override styling on box component

--- src/components/box/box.comp.tsx
+++ src/components/box/box.comp.tsx
@@ -1,7 +1,6 @@
 import { createBox } from "@dessert-box/react";
 
 import { getSprinkles } from "../../styles/utils/get_sprinkles.css";
-import { box } from "./box.styles.css";
 
 import type { SprinklesArgs } from "../../styles/utils/get_sprinkles.css";
 import type { ComponentPropsWithoutRef } from "react";
@@ -15,7 +14,7 @@ import type { ComponentPropsWithoutRef } from "react";
  */
 export const Box = createBox({
   atoms: getSprinkles,
-  defaultClassName: box,
+  // defaultClassName: box,
 });
 
 export type BoxProps = ComponentPropsWithoutRef<typeof Box> & SprinklesArgs;
