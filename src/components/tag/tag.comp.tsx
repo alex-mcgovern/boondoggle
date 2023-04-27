6e537ec305bc4a1de1b9716c57a3e9de6b2ae323
Alex McGovern <alex_mcgovernsmith@icloud.com>
2023-04-28 00:57:10 +0100

    improve(Tag): reduce fontsize for all sizes

--- src/components/tag/tag.comp.tsx
+++ src/components/tag/tag.comp.tsx
@@ -7,9 +7,9 @@ import { Box } from "../box";
 import { SlotWrapper } from "../slot_wrapper";
 import * as styles from "./tag.styles.css";
 
-import type { ElementSizeEnum } from "../../styles/common/element_size.css";
 import type { ColorOverlay } from "../../styles/theme.css";
 import type { BoxProps } from "../box";
+import type { TagSizeEnum } from "./tag.styles.css";
 import type { ReactNode } from "react";
 
 export type TagProps = BoxProps & {
@@ -30,7 +30,7 @@ export type TagProps = BoxProps & {
   /** The string URI to link to. Supports relative and absolute URIs. */
   to?: string;
   /** The size of the tag */
-  size?: ElementSizeEnum;
+  size?: TagSizeEnum;
 };
 
 export function Tag({
