23e52382d095b63255cd6b57e9c648ba0d7b8cc8
Alex McGovern <alex_mcgovernsmith@icloud.com>
2023-05-06 18:32:29 +0100

    improve(Tooltip): allow any react node

--- src/components/tooltip/tooltip.comp.tsx
+++ src/components/tooltip/tooltip.comp.tsx
@@ -4,28 +4,25 @@ import * as RadixPopover from "@radix-ui/react-popover";
 import { Box } from "../box";
 import { Card } from "../card";
 import { Icon } from "../icon";
-import * as styles from "./tooltip.styles.css";
 
-import type { IconProps } from "../icon";
+import type { ReactNode } from "react";
 
 export type TooltipProps = {
   popoverText: string;
-  icon?: IconProps["icon"];
+  children?: ReactNode;
   side: RadixPopover.PopoverContentProps["side"];
 };
 export function Tooltip({
   popoverText,
-  icon = faInfoCircle,
+  children = <Icon icon={faInfoCircle} />,
   side = "bottom",
 }: TooltipProps) {
   return (
     <RadixPopover.Root>
-      <RadixPopover.Trigger className={styles.popoverTrigger}>
-        <Icon icon={icon} />
-      </RadixPopover.Trigger>
+      <RadixPopover.Trigger asChild>{children}</RadixPopover.Trigger>
 
       <RadixPopover.Content sideOffset={1} side={side}>
-        <Card maxWidth="gridSpan5" padding="spacing1">
+        <Card maxWidth="gridSpan5" padding="spacing0" fontSize="body_xs">
           <Box>{popoverText}</Box>
         </Card>
       </RadixPopover.Content>
