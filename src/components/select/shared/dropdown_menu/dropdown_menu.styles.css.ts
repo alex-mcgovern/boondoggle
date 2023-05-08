7323a39fc7db77fa2bb291207115bbcd6c540196
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-05-08 18:59:41 +0100

    fix(dropdown menu): use absolute positioning on menu element

--- src/components/select/shared/dropdown_menu/dropdown_menu.styles.css.ts
+++ src/components/select/shared/dropdown_menu/dropdown_menu.styles.css.ts
@@ -24,6 +24,7 @@ export const dropdownListWrapper = style([
 
     /** Style as a card */
     borderRadius: "sm",
+    position: "absolute",
     overflow: "hidden",
     boxShadow: "sm",
     border: "border_default",
