19e7716d1ccc52f912d3fc771f53ea51740c8257
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-05-26 09:17:15 +0100

    fix(SlotWrapper): fix height/width

--- src/components/slot_wrapper/slot_wrapper.styles.css.ts
+++ src/components/slot_wrapper/slot_wrapper.styles.css.ts
@@ -1,5 +1,16 @@
 import { style } from "@vanilla-extract/css";
 
-export const slot = style({
-  pointerEvents: "none",
-});
+import { getSprinkles } from "../../styles/utils/get_sprinkles.css";
+
+export const slot = style(
+  {
+    pointerEvents: "none",
+  },
+  getSprinkles({
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    maxHeight: "spacing3",
+    maxWidth: "spacing3",
+  })
+);
