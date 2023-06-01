62277349e7d8b3cd06ce283a351935a76711205a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-06-01 13:19:18 +0100

    improve: full-screen modal dialog on mobile

--- src/components/dialog_modal/dialog_modal.styles.css.ts
+++ src/components/dialog_modal/dialog_modal.styles.css.ts
@@ -1,7 +1,11 @@
 import { style } from "@vanilla-extract/css";
-import { calc } from "@vanilla-extract/css-utils";
 
 import { animateAppear } from "../../styles/common/animations.css";
+import {
+  MEDIA_QUERY_DESKTOP,
+  MEDIA_QUERY_MOBILE,
+  MEDIA_QUERY_TABLET,
+} from "../../styles/common/media_queries.css";
 import { getSprinkles } from "../../styles/utils/get_sprinkles.css";
 
 export const dialogOverlay = style([
@@ -26,15 +30,27 @@ export const dialogContent = style([
     textDecoration: "none",
   }),
   {
+    "@media": {
+      [MEDIA_QUERY_DESKTOP]: {
+        height: "100dvh",
+        width: "50vw",
+      },
+      [MEDIA_QUERY_MOBILE]: {
+        height: "100dvh",
+        width: "100vw",
+      },
+      [MEDIA_QUERY_TABLET]: {
+        maxHeight: "75dvh",
+        width: "75vw",
+      },
+    },
     left: "50%",
-    maxWidth: calc.subtract("100vw", "spacing2"),
     position: "fixed",
     selectors: {
       "&:focus": { outline: "none" },
     },
     top: "50%",
     transform: "translate(-50%, -50%)",
-    width: "50%",
     zIndex: 99999,
   },
   animateAppear,
