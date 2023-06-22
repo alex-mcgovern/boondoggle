5f8bcadc32de6f177cbbf78a9b2ee4b509fea805
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-06-22 16:35:40 +0100

    improve(Tooltip): forward ref

--- src/components/tooltip/tooltip.comp.tsx
+++ src/components/tooltip/tooltip.comp.tsx
@@ -1,11 +1,11 @@
 import { faInfoCircle } from "@fortawesome/pro-light-svg-icons";
+import { type ReactNode, forwardRef } from "react";
 
 import { Dialog } from "../dialog";
 import { Icon } from "../icon";
 import { tooltipTextStyle, tooltipTriggerStyle } from "./tooltip.styles.css";
 
 import type { DialogPlacementEnum } from "../dialog/dialog.styles.css";
-import type { ReactNode } from "react";
 
 export type TooltipProps = {
   children?: ReactNode;
@@ -13,23 +13,29 @@ export type TooltipProps = {
   tooltipText: string;
 };
 
-export function Tooltip({
-  tooltipText,
-  placement = "top-center",
-  children = (
-    <button className={tooltipTriggerStyle} type="button">
-      <Icon icon={faInfoCircle} />
-    </button>
-  ),
-}: TooltipProps) {
-  return (
-    <Dialog
-      className={tooltipTextStyle}
-      openOn="hover"
-      placement={placement}
-      triggerNode={children}
-    >
-      {tooltipText}
-    </Dialog>
-  );
-}
+export const Tooltip = forwardRef<HTMLDialogElement, TooltipProps>(
+  (
+    {
+      tooltipText,
+      placement = "top-center",
+      children = (
+        <button className={tooltipTriggerStyle} type="button">
+          <Icon icon={faInfoCircle} />
+        </button>
+      ),
+    },
+    ref
+  ) => {
+    return (
+      <Dialog
+        className={tooltipTextStyle}
+        openOn="hover"
+        placement={placement}
+        ref={ref}
+        triggerNode={children}
+      >
+        {tooltipText}
+      </Dialog>
+    );
+  }
+);
