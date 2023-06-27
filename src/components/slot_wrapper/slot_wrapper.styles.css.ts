dc247ce571fe8c27868f89f899283dfc1352c38a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-06-27 14:50:11 +0100

    improve(SlotWrapper): ensure slots don't change size

--- src/components/slot_wrapper/slot_wrapper.styles.css.ts
+++ src/components/slot_wrapper/slot_wrapper.styles.css.ts
@@ -4,9 +4,9 @@ import { recipe } from "@vanilla-extract/recipes";
 import { getSprinkles } from "../../styles/utils/get_sprinkles.css";
 
 const slotSize = styleVariants({
-  lg: [getSprinkles({ maxHeight: "spacing_3", maxWidth: "spacing_3" })],
-  md: [getSprinkles({ maxHeight: "spacing_2", maxWidth: "spacing_2" })],
-  sm: [getSprinkles({ maxHeight: "spacing_1.5", maxWidth: "spacing_1.5" })],
+  lg: [getSprinkles({ height: "spacing_3", width: "spacing_3" })],
+  md: [getSprinkles({ height: "spacing_2", width: "spacing_2" })],
+  sm: [getSprinkles({ height: "spacing_1.5", width: "spacing_1.5" })],
 });
 
 export const getSlotStyles = recipe({
@@ -18,8 +18,6 @@ export const getSlotStyles = recipe({
       alignItems: "center",
       display: "flex",
       justifyContent: "center",
-      maxHeight: "spacing_3",
-      maxWidth: "spacing_3",
     }),
   ],
   variants: {
