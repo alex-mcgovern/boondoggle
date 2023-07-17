7c0d9295fcb862a513ad36780ee2c0a2037769f0
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-07-17 20:49:29 +0100

    improve(RadioButtonCard): use square card layout

--- src/components/radio_button_cards/radio_button_cards.styles.css.ts
+++ src/components/radio_button_cards/radio_button_cards.styles.css.ts
@@ -2,6 +2,7 @@ import { style } from "@vanilla-extract/css";
 
 import { a11yFocus, a11yFocusStyleRule } from "../../styles/common/a11y.css";
 import { vars } from "../../styles/theme.css";
+import { createAccessibleTransition } from "../../styles/utils/create_accessible_transition";
 import { getSprinkles } from "../../styles/utils/get_sprinkles.css";
 
 /** ----------------------------------------------------------------------------- */
@@ -33,24 +34,32 @@ export const radioButtonLabelStyles = style([
   getSprinkles({
     alignItems: "center",
     aspectRatio: "square",
+    border: "border_default",
     borderRadius: "sm",
     display: "flex",
     gap: "spacing_2",
+    justifyContent: "center",
     padding: "spacing_2",
     width: "100%",
   }),
   {
+    ...createAccessibleTransition({
+      transition: `border-color ${vars.transitionDuration.short} ease, background ${vars.transitionDuration.short} ease`,
+    }),
     accentColor: vars.color.button_default,
     maxWidth: "12rem",
     selectors: {
       "&:has(input:checked)": {
         background: vars.color.tint_default,
+        border: `1px solid ${vars.color.border_active}`,
       },
       "&:has(input:focus)": {
         ...a11yFocusStyleRule,
+        border: `1px solid ${vars.color.button_default}`,
       },
       "&:hover": {
         background: vars.color.tint_default,
+        border: `1px solid ${vars.color.border_active}`,
         cursor: "pointer",
       },
       "&:not(:last-child)": {
