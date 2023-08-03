158d569c628f4191e867c5dd6c509b22d15d3452
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-08-03 12:43:45 +0100

    improve(SelectSingle): add controlled isOpen state

--- src/components/select/select.types.ts
+++ src/components/select/select.types.ts
@@ -2,6 +2,7 @@ import type { ElementSizeEnum } from "../../styles/common/element_size.css";
 import type { ConditionalLabelProps } from "../../types";
 import type { BoxProps } from "../box";
 import type { InputProps } from "../input/input.comp";
+import type { UseComboboxStateChange } from "downshift";
 import type { ReactNode } from "react";
 
 export type DropdownItemShape = BoxProps & {
@@ -18,8 +19,10 @@ export type SelectCommonProps = ConditionalLabelProps & {
   inputProps?: Partial<InputProps>;
   invalid?: boolean;
   isFilterable?: boolean;
+  isOpen?: boolean;
   items: Array<DropdownItemShape>;
   name: string;
+  onIsOpenChange?: (changes: UseComboboxStateChange<DropdownItemShape>) => void;
   size?: ElementSizeEnum;
   slotLeft?: ReactNode;
   slotRight?: ReactNode;
