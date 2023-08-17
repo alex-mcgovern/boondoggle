a143a3e2160a9479dc6b6bd5073ddaca8d3e886a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-08-17 12:40:03 +0100

    fix(Slot): make all slots optional

--- src/types.ts
+++ src/types.ts
@@ -178,9 +178,9 @@ export type WithSize = {
 
 export type WithSlots = {
   /** React node(s) rendered on the left-hand side. */
-  slotLeft?: [ReactNode, ReactNode?, ReactNode?];
+  slotLeft?: [ReactNode?, ReactNode?, ReactNode?];
   /** React node(s) rendered on the right-hand side. */
-  slotRight?: [ReactNode, ReactNode?, ReactNode?];
+  slotRight?: [ReactNode?, ReactNode?, ReactNode?];
 };
 
 /** ----------------------------------------------------------------------------- */
