43353046ea9ac5f8832ed744a45fe9ef27da128f
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-08-18 14:30:43 +0100

    chore: tweak tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,17 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 import { defineConfig } from "tsup";
 
+const env = process.env.NODE_ENV;
+
 export default defineConfig({
   bundle: false,
   clean: true,
   config: "./tsconfig.build.json",
-  dts: true,
-  entry: ["src/index.ts"],
+  dts: {
+    entry: "./src/index.ts",
+    resolve: true,
+  },
+  entry: ["src/**/index.(ts|tsx)", "src/**/*.css.ts"],
   esbuildPlugins: [
     vanillaExtractPlugin({
       identifiers: "short",
@@ -14,12 +19,13 @@ export default defineConfig({
       runtime: false,
     }),
   ],
-  format: "esm",
-  minify: true,
-  outDir: "dist",
-  sourcemap: true,
+  format: ["esm", "cjs"],
+  minify: env === "production",
+  outDir: env === "production" ? "dist" : "lib",
+  sourcemap: env === "prod",
   splitting: true,
   target: "es2020",
   treeshake: true,
   tsconfig: "./tsconfig.build.json",
+  watch: env === "development",
 });
