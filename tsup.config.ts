db9de0eeb10ccbc9531c96b83a0f6c114f4cc1ac
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-08-18 14:33:57 +0100

    chore: tweak tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       runtime: false,
     }),
   ],
-  format: ["esm", "cjs"],
+  format: "esm",
   minify: env === "production",
   outDir: env === "production" ? "dist" : "lib",
   sourcemap: env === "prod",
