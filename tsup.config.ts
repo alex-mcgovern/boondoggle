3150f720805c1ba500aa9ff0dd49bbd4254b5539
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-08-23 08:18:03 +0100

    fix(tsup): don't bundle dependencies or dependency types

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,24 +2,25 @@ import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  bundle: true,
+  bundle: false,
   clean: true,
   config: "./tsconfig.build.json",
   dts: {
     entry: "./src/index.ts",
-    resolve: true,
+    resolve: false,
   },
+  // dts: true,
   entry: ["src/**/index.(ts|tsx)", "src/**/*.css.ts"],
-  esbuildPlugins: [
+  format: ["esm", "cjs"],
+  minify: true,
+  outDir: "dist",
+  plugins: [
     vanillaExtractPlugin({
       identifiers: "short",
       outputCss: true,
       runtime: false,
     }),
   ],
-  format: ["esm", "cjs"],
-  minify: true,
-  outDir: "dist",
   sourcemap: true,
   splitting: true,
   target: "es2020",
