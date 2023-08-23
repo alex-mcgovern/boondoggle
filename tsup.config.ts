892dfd77f7a4cbad6d7c179700480b566f15b740
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-08-23 08:47:20 +0100

    improve(tsup): revert to older version

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,29 +2,24 @@ import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  bundle: false,
+  bundle: true,
   clean: true,
   config: "./tsconfig.build.json",
   dts: {
     entry: "./src/index.ts",
     resolve: false,
   },
-  // dts: true,
   entry: ["src/**/index.(ts|tsx)", "src/**/*.css.ts"],
   esbuildPlugins: [
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     vanillaExtractPlugin({
       identifiers: "short",
       outputCss: true,
       runtime: false,
     }),
   ],
-
   format: ["esm", "cjs"],
   minify: true,
   outDir: "dist",
-
   sourcemap: true,
   splitting: true,
   target: "es2020",
