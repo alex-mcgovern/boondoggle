31c374199ff66238bcdbe1271698ef896352378d
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-09-06 19:42:53 +0100

    fix(css selectors): pick up active & disabled states correctly

--- src/styles/common/selectors.css.ts
+++ src/styles/common/selectors.css.ts
@@ -1,6 +1,6 @@
 const SELECTOR_IS_LINK_BUTTON_INPUT = ":is(a,button,input)";
 
-const SELECTOR_IS_NOT_DISABLED = ":not([disabled])";
+const SELECTOR_IS_NOT_DISABLED = ":not(:is([disabled], [aria-disabled='true']))";
 
 export const SELECTOR_IS_HOVER = `:is(:hover)`;
 
@@ -8,7 +8,7 @@ export const SELECTOR_IS_FOCUS = `:is(:focus-visible)`;
 
 const SELECTOR_IS_HOVER_FOCUS = `:is(:hover, :focus-visible)`;
 
-const SELECTOR_IS_ACTIVE = `:active`;
+const SELECTOR_IS_ACTIVE = `is(:active, [data-active='true'])`;
 
 /**
  * Button/link/input: hover
