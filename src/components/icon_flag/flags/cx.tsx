e8aa704c1cf04cf1628587ec77d2e4b0e4cdf935
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-09-12 14:15:54 +0100

    improve(icon flag): make styles less repetitive

--- src/components/icon_flag/flags/cx.tsx
+++ src/components/icon_flag/flags/cx.tsx
@@ -1,5 +1,8 @@
+import clsx from "clsx";
+
 import { getSprinkles } from "../../../styles/utils/get_sprinkles.css";
 import { Box } from "../../box";
+import { flagStyles } from "../styles.css";
 
 import type { SprinklesArgs } from "../../../styles/utils/get_sprinkles.css";
 
@@ -7,13 +10,7 @@ export function FlagCx({ height = "space_8", width = "space_8", ...rest }: Sprin
     return (
         <Box
             as="svg"
-            className={getSprinkles({
-                border: "border_default",
-                borderRadius: "50%",
-                height,
-                width,
-                ...rest,
-            })}
+            className={clsx(flagStyles, getSprinkles({ height, width, ...rest }))}
             viewBox="0 0 512 512"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
---
+++ src/components/icon_flag/styles.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+import { getSprinkles } from "../../styles/utils/get_sprinkles.css";
+
+export const flagStyles = style([
+    getSprinkles({
+        border: "border_default",
+        borderRadius: "50%",
+        flexShrink: "0",
+    }),
+]);
