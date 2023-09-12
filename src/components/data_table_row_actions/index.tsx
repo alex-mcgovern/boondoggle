2471fdd736cf913c26de7678ede1ed62634337f8
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-09-12 11:10:35 +0100

    fix(DataTable row actions): prevent click on row when click on action button

--- src/components/data_table_row_actions/index.tsx
+++ src/components/data_table_row_actions/index.tsx
@@ -5,6 +5,7 @@ import { SelectButton } from "../select/select_button";
 
 import type { SelectButtonProps } from "../select/select_button";
 import type { SelectItemShape } from "../select/types";
+import type { ChangeEvent } from "react";
 
 export type DataTableRowActionsProps = {
     /**
@@ -19,15 +20,21 @@ export type DataTableRowActionsProps = {
 export function DataTableRowActions({ items }: DataTableRowActionsProps) {
     return (
         <SelectButton
+            // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
             buttonProps={{
                 appearance: "secondary",
                 name: "select_button_data_table_team",
+                onClick: (e: ChangeEvent<HTMLButtonElement>) => {
+                    return e.preventDefault(); // Prevent triggering the row click event if there is one
+                },
                 size: "square_md",
             }}
             items={items}
             name="select_button_data_table_team"
             placement="bottom-end"
+            // eslint-disable-next-line react-perf/jsx-no-new-array-as-prop
             slotRight={[<Icon icon={faEllipsis} />]}
+            // eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
             wrapperProps={{
                 marginLeft: "auto",
                 width: "min-content",
