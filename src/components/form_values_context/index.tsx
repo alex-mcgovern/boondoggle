45eb11b4bdb0c57cc21d5cd71584c8601b4f0814
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-09-20 15:14:30 +0100

    improve(FormValuesContextProvider): fix context value

--- src/components/form_values_context/index.tsx
+++ src/components/form_values_context/index.tsx
@@ -4,19 +4,19 @@ import { useFormContext } from "react-hook-form";
 import type { ReactNode } from "react";
 import type { FieldValues } from "react-hook-form";
 
-export const FormValuesContext = createContext<FieldValues | undefined>(undefined);
+export const FormValuesContext = createContext<{ field_values: FieldValues | undefined }>({
+    field_values: undefined,
+});
 
 export type FormValuesContextProviderProps = {
     children: ReactNode;
 };
 
-export function FormValuesContextProvider<TFieldValues extends FieldValues>({
-    children,
-}: FormValuesContextProviderProps) {
-    const { getValues } = useFormContext<TFieldValues>();
+export function FormValuesContextProvider({ children }: FormValuesContextProviderProps) {
+    const { getValues } = useFormContext();
 
     const values = useMemo(() => {
-        return getValues();
+        return { field_values: getValues() };
     }, [getValues]);
 
     return <FormValuesContext.Provider value={values}>{children}</FormValuesContext.Provider>;
