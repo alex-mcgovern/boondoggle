e13c5bc3ec64639b6a47b9fc31d192ac04d365a7
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-10-05 16:50:11 +0100

    improve(tsup): add `use client` directive to each component via banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,14 +2,24 @@ import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    bundle: true,
+    banner: {
+        js: '"use client"',
+    },
+    bundle: false,
     clean: true,
     config: "./tsconfig.build.json",
     dts: {
         entry: "./src/index.ts",
         resolve: false,
     },
-    entry: ["src/index.ts"],
+    entry: [
+        "src/**/*.{ts,tsx}",
+        "!src/**/*stories*",
+        "!src/**/*test*",
+        "!src/**/*mock*",
+        "!src/**/*.css.ts",
+        "!src/**/*.css.ts.vanilla.css*",
+    ],
     esbuildPlugins: [
         vanillaExtractPlugin({
             identifiers: "debug",
@@ -17,13 +27,14 @@ export default defineConfig({
             runtime: false,
         }),
     ],
+    external: ["react"],
     format: ["esm", "cjs"],
+    injectStyle: false,
     minify: false,
     outDir: "dist",
     platform: "browser",
     sourcemap: true,
-    splitting: true,
+    splitting: false,
     target: "es2020",
-    treeshake: true,
     tsconfig: "./tsconfig.build.json",
 });
