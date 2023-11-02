b8d2ed5889cfec1ee505c22b1ee3329248ad30ab
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-11-02 18:47:17 +0000

    chore: tidy-ups

--- src/components/native-select/index.tsx
+++ src/components/native-select/index.tsx
@@ -1,109 +1,107 @@
 import { extractAtomsFromProps } from "@dessert-box/core";
 import clsx from "clsx";
 import { forwardRef } from "react";
+import type { ComponentPropsWithoutRef } from "react";
 import { getOptionalLabelProps } from "../../common-types";
+import type {
+	WithColorOverlay,
+	WithDescription,
+	WithHideLastpass,
+	WithName,
+	WithOptionalIsVisibilityToggleable,
+	WithOptionalLabel,
+	WithOptionalPlaceholder,
+	WithReadOnly,
+	WithSize,
+	WithSlots,
+	WithStateInvalid,
+	WithWrapperProps,
+} from "../../common-types";
 import { a11yError } from "../../styles/common/a11y.css";
 import { utilCss } from "../../styles/utils/util_css.css";
+import type { UtilCssArgs } from "../../styles/utils/util_css.css";
 import { FieldWrapper } from "../field_wrapper";
 import * as styles from "./styles.css";
-import type { ComponentPropsWithoutRef } from "react";
-import type {
-    WithColorOverlay,
-    WithDescription,
-    WithHideLastpass,
-    WithName,
-    WithOptionalIsVisibilityToggleable,
-    WithOptionalLabel,
-    WithOptionalPlaceholder,
-    WithReadOnly,
-    WithSize,
-    WithSlots,
-    WithStateInvalid,
-    WithWrapperProps,
-} from "../../common-types";
-import type { UtilCssArgs } from "../../styles/utils/util_css.css";
 
 export type NativeSelectProps = Omit<
-    ComponentPropsWithoutRef<"select">,
-    "color" | "ref" | "id" | "size"
+	ComponentPropsWithoutRef<"select">,
+	"color" | "ref" | "id" | "size"
 > &
-    // WithOptionalIsClearable &
-    // WithOptionalIsCopyable &
-    UtilCssArgs &
-    WithColorOverlay &
-    WithDescription &
-    WithHideLastpass &
-    WithOptionalIsVisibilityToggleable &
-    WithName &
-    WithOptionalLabel &
-    WithOptionalPlaceholder &
-    WithReadOnly &
-    WithSize &
-    WithSlots &
-    WithStateInvalid &
-    WithWrapperProps & { children: React.ReactNode };
+	// WithOptionalIsClearable &
+	// WithOptionalIsCopyable &
+	UtilCssArgs &
+	WithColorOverlay &
+	WithDescription &
+	WithHideLastpass &
+	WithOptionalIsVisibilityToggleable &
+	WithName &
+	WithOptionalLabel &
+	WithOptionalPlaceholder &
+	WithReadOnly &
+	WithSize &
+	WithSlots &
+	WithStateInvalid &
+	WithWrapperProps & { children: React.ReactNode };
 
 export const NativeSelect = forwardRef<HTMLSelectElement, NativeSelectProps>(
-    (
-        {
-            colorOverlay,
-            description,
-            errorMessage,
-            hideLastpass,
-            invalid,
-            isLabelVisible,
-            label,
-            labelProps,
-            labelTooltip,
-            children,
-            name,
-            size = "sm",
-            wrapperProps,
-            ...rest
-        },
-        ref,
-    ) => {
-        /**
-         * Separate `UtilCssArgs` from other spread props, so we don't break Vanilla Extract
-         */
-        const { atomProps, otherProps } = extractAtomsFromProps(rest, utilCss);
+	(
+		{
+			colorOverlay,
+			description,
+			errorMessage,
+			hideLastpass,
+			invalid,
+			isLabelVisible,
+			label,
+			labelProps,
+			labelTooltip,
+			children,
+			name,
+			size = "sm",
+			wrapperProps,
+			...rest
+		},
+		ref,
+	) => {
+		/**
+		 * Separate `UtilCssArgs` from other spread props, so we don't break Vanilla Extract
+		 */
+		const { atomProps, otherProps } = extractAtomsFromProps(rest, utilCss);
 
-        return (
-            <FieldWrapper
-                colorOverlay={colorOverlay}
-                description={description}
-                errorMessage={errorMessage}
-                hideLastpass={hideLastpass}
-                invalid={invalid}
-                wrapperProps={wrapperProps}
-                {...getOptionalLabelProps({
-                    isLabelVisible,
-                    label,
-                    labelProps,
-                    labelTooltip,
-                    name,
-                })}
-            >
-
-                <select
-                    aria-invalid={invalid}
-                    aria-label={label}
-                    className={clsx(
-                        styles.select({ size }),
-                        utilCss(atomProps),
-                        {
-                            [a11yError]: invalid,
-                        },
-                    )}
-                    id={name}
-                    name={name}
-                    ref={ref}
-                    {...otherProps}
-                >
-                    {children}
-                </select>
-
-            </FieldWrapper>
-        );
-    },
+		return (
+			<FieldWrapper
+				colorOverlay={colorOverlay}
+				description={description}
+				errorMessage={errorMessage}
+				hideLastpass={hideLastpass}
+				invalid={invalid}
+				wrapperProps={wrapperProps}
+				{...getOptionalLabelProps({
+					isLabelVisible,
+					label,
+					labelProps,
+					labelTooltip,
+					name,
+				})}
+			>
+				<select
+					aria-invalid={invalid}
+					aria-label={label}
+					className={clsx(
+						styles.select({ size }),
+						utilCss(atomProps),
+						{
+							[a11yError]: invalid,
+						},
+					)}
+					id={name}
+					name={name}
+					ref={ref}
+					{...otherProps}
+				>
+					{children}
+				</select>
+			</FieldWrapper>
+		);
+	},
 );
--- src/components/native-select/stories.tsx
+++ src/components/native-select/stories.tsx
@@ -3,22 +3,23 @@ import { NativeSelect as StoryComp } from ".";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
-    title: "Components/NativeSelect",
-    component: StoryComp,
-    args: {},
+	title: "Components/NativeSelect",
+	component: StoryComp,
+	args: {},
 } satisfies Meta<typeof StoryComp>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        name: "foo",
-        children: (<>
-            <option value="foo">Foo</option>
-            <option value="bar">Bar</option>
-            <option value="baz">Baz</option>
-
-        </>)
-    }
+	args: {
+		name: "foo",
+		children: (
+			<>
+				<option value="foo">Foo</option>
+				<option value="bar">Bar</option>
+				<option value="baz">Baz</option>
+			</>
+		),
+	},
 };
