d8b3f4f59c92b09b1170acbe2514bb4d997b2bcc
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-11-14 11:07:56 +0000

    chore: tidy-ups

--- src/components/data_table/_components/filter-pill/FilterPillGroup.tsx
+++ src/components/data_table/_components/filter-pill/FilterPillGroup.tsx
@@ -4,23 +4,16 @@ import * as styles from "./FilterPillGroup.css";
 /**
  * @private This is an internal component not intended for public use.
  */
-export const PrivateDataTableFilterPillGroup = (
+export const FilterPillGroup = React.forwardRef<
+	HTMLDivElement,
 	{
-		children,
-		isFiltered,
-	}: {
 		children: React.ReactNode;
 		isFiltered: boolean;
-	},
-	ref: React.ForwardedRef<HTMLDivElement>,
-) => {
+	}
+>(({ children, isFiltered }, ref) => {
 	return (
 		<div ref={ref} className={styles.pillGroup({ isFiltered })}>
 			{children}
 		</div>
 	);
-};
-
-export const FilterPillGroup = React.forwardRef(
-	PrivateDataTableFilterPillGroup,
-);
+});
