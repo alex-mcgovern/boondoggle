76b5cee0a991ce92ca708ed48aebaa90bac021f0
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-11-17 09:54:43 +0000

    improve: recursively generate .dts files for library

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,45 @@
 // import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 import { defineConfig } from "tsup";
+import fs from "fs";
+import path from "path";
+
+/** -----------------------------------------------------------------------------
+ * UTIL FOR GETTING THE FILES WE WANT TO GENERATE .DTS FILES FOR
+ * ------------------------------------------------------------------------------- */
+
+const isIncluded = (file: string) =>
+	file.includes(".tsx") || file.includes("index.css.ts");
+const isExcluded = (file: string) =>
+	file.includes("_lib") ||
+	file.includes("_components") ||
+	file.includes("test") ||
+	file.includes("stories") ||
+	file.includes("spec") ||
+	file.includes("mock") ||
+	file.includes("types");
+
+const files: string[] = [];
+
+const getDtsFiles = (directory: string) => {
+	const filesInDirectory = fs.readdirSync(directory);
+	for (const file of filesInDirectory) {
+		const absolute = path.join(directory, file);
+		if (fs.statSync(absolute).isDirectory()) {
+			/** @note Use of recursion */
+			getDtsFiles(absolute);
+		} else {
+			if (isIncluded(absolute) && !isExcluded(absolute)) {
+				files.push(absolute);
+			}
+		}
+	}
+};
+
+getDtsFiles("./src");
+
+/** -----------------------------------------------------------------------------
+ * TSUP CONFIG
+ * ------------------------------------------------------------------------------- */
 
 export default defineConfig({
 	banner: {
@@ -9,8 +49,10 @@ export default defineConfig({
 	clean: true,
 	config: "./tsconfig.build.json",
 	dts: {
-		entry: "src/*.ts",
+		entry: files,
+		resolve: false,
 	},
+	// dts: true,
 	entry: [
 		"./src",
 		"!./src/**/*mock.*",
