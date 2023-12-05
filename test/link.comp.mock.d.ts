c3d3dd35434440cfd641a1211b497097b3d1999b
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-12-05 01:08:01 +0000

    chore: remove junk

--- test/link.comp.mock.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from "react";
-type LinkProps = {
-    children: ReactNode;
-    href: string;
-    id?: string;
-};
-export declare function Link({ children, href, ...rest }: LinkProps): import("react/jsx-runtime").JSX.Element;
-export {};
--- test/select-from-single-select.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ByRoleMatcher, ByRoleOptions } from "@testing-library/dom";
-export declare const selectFromSingleSelect: ({ expectedValue, getByRole, itemLabel, selectLabel, }: {
-    expectedValue: string;
-    getByRole: (role: ByRoleMatcher, options?: ByRoleOptions | undefined) => HTMLElement;
-    itemLabel: string;
-    selectLabel: string;
-}) => Promise<void>;
