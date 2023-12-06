459616eb16015277f3a1720f67ef70e863eea483
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-12-06 08:51:59 +0000

    chore: tidy ups

--- src/tag/styles.css.ts
+++ src/tag/styles.css.ts
@@ -1,11 +1,6 @@
 import { recipe } from "@vanilla-extract/recipes";
 import { withPrefersMotion } from "../css-utils";
-import {
-	SELECTOR_LINK_BUTTON_INPUT_HOVER_FOCUS,
-	a11yFocus,
-	variantColorOverlay,
-	vars,
-} from "../index.css";
+import { a11yFocus, variantColorOverlay, vars } from "../index.css";
 import { sprinkles } from "../sprinkles/index.css";
 
 export const getTagStyle = recipe({
