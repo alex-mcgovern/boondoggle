7644d5a97edaee82a1719b4d532afd4a6aef3b94
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-12-07 18:17:15 +0000

    tweak dialog alert styles

--- src/v2-dialog-alert/styles.css.ts
+++ src/v2-dialog-alert/styles.css.ts
@@ -10,7 +10,6 @@ export const dialogAlertWrapperCSS = recipe({
 			alignItems: "start",
 
 			background: "tint_default",
-			// border: "border_default",
 			gap: "space_2",
 
 			paddingX: "space_4",
@@ -18,11 +17,6 @@ export const dialogAlertWrapperCSS = recipe({
 
 			zIndex: "1",
 		}),
-		// {
-		// 	marginTop: "-1px",
-		// 	marginLeft: "-1px",
-		// 	marginRight: "-1px",
-		// },
 	],
 	variants: {
 		colorOverlay: variantColorOverlay,
