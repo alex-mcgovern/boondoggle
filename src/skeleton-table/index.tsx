7dfbcd4e34c314e65f2c5c9dba908a30d65f3c2b
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-12-07 16:31:00 +0000

    improve: table skeleton

--- src/skeleton-table/index.tsx
+++ src/skeleton-table/index.tsx
@@ -10,6 +10,24 @@ export const SkeletonTable = () => {
 	return (
 		<>
 			<Box
+				gap="space_2"
+				display="flex"
+				alignItems="center"
+				marginY="space_3"
+			>
+				<Skeleton height="space_8" borderRadius="md" __width="12rem" />
+				{Array.from({ length: 4 }).map(() => {
+					return (
+						<Skeleton
+							height="space_8"
+							borderRadius="pill"
+							__width={`${random(4, 8)}rem`}
+						/>
+					);
+				})}
+			</Box>
+			<Box
+				borderTop="border_default"
 				display="grid"
 				__gridTemplateColumns="2fr min-content min-content min-content"
 			>
