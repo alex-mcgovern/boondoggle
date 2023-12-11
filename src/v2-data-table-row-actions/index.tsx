2c826a83f2c513d38e7ad9ed063ab3007b87427f
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-12-11 09:05:44 +0000

    improve(v2 row actions): styles

--- src/v2-data-table-row-actions/index.tsx
+++ src/v2-data-table-row-actions/index.tsx
@@ -22,6 +22,7 @@ export function V2DataTableRowActions<TActionId extends string>({
 			}}
 			size="sm"
 			buttonProps={{
+				appearance: "ghost",
 				name: "row_actions",
 				children: <Icon icon={faEllipsis} />,
 			}}
