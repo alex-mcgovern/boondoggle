5b972905d9df6882cceb80c6bc969d1ba7b7a38a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2023-12-18 16:08:01 +0000

    feat(scrollable main): re-add collapsible functionality

--- src/v2-scrollable-main/index.tsx
+++ src/v2-scrollable-main/index.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import type { HTMLProps } from "react";
 import { WithSize } from "../types";
+import { useCollapsibleSideNav } from "../v2-collapsible-side-nav";
 import { scrollableMainCSS, scrollableMainInnerCSS } from "./styles.css";
 import { nonScrollableWrapperCSS } from "./styles.css";
 
@@ -29,12 +30,16 @@ export function ScrollableMain({
 		 */
 		headers: "0" | "1" | "2";
 	}) {
+	const [isSidebarOpen] = useCollapsibleSideNav();
+
 	return (
 		<main
 			className={clsx(className, scrollableMainCSS({ headers }))}
 			{...rest}
 		>
-			<div className={scrollableMainInnerCSS({ size })}>{children}</div>
+			<div className={scrollableMainInnerCSS({ size, isSidebarOpen })}>
+				{children}
+			</div>
 		</main>
 	);
 }
--- src/v2-scrollable-main/styles.css.ts
+++ src/v2-scrollable-main/styles.css.ts
@@ -1,8 +1,11 @@
-import { style } from "@vanilla-extract/css";
+import { createContainer, style } from "@vanilla-extract/css";
 import { calc } from "@vanilla-extract/css-utils";
 import { recipe } from "@vanilla-extract/recipes";
+import { withPrefersMotion } from "../css-utils";
 import { vars } from "../index.css";
 import { sprinkles } from "../sprinkles/index.css";
+
+const scrollableContainer = createContainer();
 
 export const nonScrollableWrapperCSS = style({
 	height: "100dvh",
@@ -12,13 +15,14 @@ export const nonScrollableWrapperCSS = style({
 export const scrollableMainCSS = recipe({
 	base: [
 		sprinkles({
-			paddingX: "space_4",
 			overflowY: "auto",
 			overflowX: "hidden",
 			height: "100dvh",
 			width: "100%",
 		}),
 		{
+			containerName: scrollableContainer,
+			containerType: "inline-size",
 			scrollbarGutter: "stable",
 		},
 	],
@@ -44,16 +48,142 @@ export const scrollableMainCSS = recipe({
 	},
 });
 
+const positive = (value: string) => {
+	return `max(${value}, 0px)`;
+};
+
+const calcNonOffsetMargin = (contentWidth: string) => {
+	return positive(calc.subtract("50vw", calc.divide(contentWidth, 2)));
+};
+
+const calcOffsetForSidebarLeft = (contentWidth: string) => {
+	return positive(
+		calc.subtract("50vw", calc.divide(contentWidth, 2), vars.width.sideBar),
+	);
+};
+
+const calcOffsetForSidebarRight = (contentWidth: string) => {
+	return positive(calc.subtract("50vw", calc.divide(contentWidth, 2)));
+};
+
 export const scrollableMainInnerCSS = recipe({
 	base: [
 		sprinkles({
+			position: "relative",
 			marginX: "auto",
+			width: "100%",
+			maxWidth: "main_lg",
+			padding: "space_4",
+		}),
+		withPrefersMotion({
+			// transitionProperty: "margin, max-width",
+			// transitionDuration: vars.transitionDuration.sideBarShowHide,
+			// transitionTimingFunction: vars.ease.quart_in_out,
 		}),
 	],
 	defaultVariants: {
+		isSidebarOpen: true,
 		size: "lg",
 	},
+	compoundVariants: [
+		/**
+		 * Size lg
+		 */
+
+		{
+			variants: {
+				size: "lg",
+				isSidebarOpen: true,
+			},
+			style: [
+				{
+					marginLeft: calcOffsetForSidebarLeft(vars.width.main_lg),
+					marginRight: calcOffsetForSidebarRight(vars.width.main_lg),
+				},
+			],
+		},
+		{
+			variants: {
+				size: "lg",
+				isSidebarOpen: false,
+			},
+			style: [
+				{
+					marginLeft: calcNonOffsetMargin(vars.width.main_lg),
+					marginRight: calcNonOffsetMargin(vars.width.main_lg),
+				},
+			],
+		},
+
+		/**
+		 * Size md
+		 */
+
+		{
+			variants: {
+				size: "md",
+				isSidebarOpen: true,
+			},
+			style: [
+				{
+					marginLeft: calcOffsetForSidebarLeft(vars.width.main_md),
+					marginRight: calcOffsetForSidebarRight(vars.width.main_md),
+				},
+			],
+		},
+		{
+			variants: {
+				size: "md",
+				isSidebarOpen: false,
+			},
+			style: [
+				{
+					marginLeft: calcNonOffsetMargin(vars.width.main_md),
+					marginRight: calcNonOffsetMargin(vars.width.main_md),
+				},
+			],
+		},
+
+		/**
+		 * Size sm
+		 */
+
+		{
+			variants: {
+				size: "sm",
+				isSidebarOpen: true,
+			},
+			style: [
+				{
+					marginLeft: calcOffsetForSidebarLeft(vars.width.main_sm),
+					marginRight: calcOffsetForSidebarRight(vars.width.main_sm),
+				},
+			],
+		},
+		{
+			variants: {
+				size: "sm",
+				isSidebarOpen: false,
+			},
+			style: [
+				{
+					marginLeft: calcNonOffsetMargin(vars.width.main_sm),
+					marginRight: calcNonOffsetMargin(vars.width.main_sm),
+				},
+			],
+		},
+	],
 	variants: {
+		isSidebarOpen: {
+			true: [
+				// withPrefersMotion({
+				// 	transitionProperty: "margin",
+				// 	transitionDuration: vars.transitionDuration.sideBarShowHide,
+				// 	transitionTimingFunction: vars.ease.quart_in_out,
+				// }),
+			],
+			false: [],
+		},
 		size: {
 			lg: [sprinkles({ maxWidth: "main_lg" })],
 			md: [sprinkles({ maxWidth: "main_md" })],
