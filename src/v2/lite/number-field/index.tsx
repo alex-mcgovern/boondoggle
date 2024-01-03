a1cb1fd52d9c31d395114ae52ce3f53d532249fd
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-01-03 15:14:02 +0000

    feat: add NumberField & FormNumberField components

---
+++ src/v2/lite/number-field/index.tsx
@@ -0,0 +1,8 @@
+import {
+	NumberField as RACNumberField,
+	NumberFieldProps as RACNumberFieldProps,
+} from "react-aria-components";
+
+export const NumberField = (props: RACNumberFieldProps) => {
+	return <RACNumberField {...props} />;
+};
