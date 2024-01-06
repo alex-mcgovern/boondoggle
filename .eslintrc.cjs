99026ffb6c2370bc8bf195d931228935cc771a2e
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-01-06 17:33:42 +0000

    chore: add react-hooks eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
         "plugin:perfectionist/recommended-natural",
         "prettier",
     ],
