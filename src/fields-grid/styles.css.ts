79d1c85ee45853c1c87da58297ae4c3db5a3e263
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-01-07 17:19:09 +0000

    fix(fields-grid): styles

--- src/fields-grid/styles.css.ts
+++ src/fields-grid/styles.css.ts
@@ -1,14 +1,17 @@
 import { style } from "@vanilla-extract/css";
 
 import { css } from "../css/index.css";
+import { vars } from "../index.css";
 
 export const fieldsGridCSS = style([
     css({
         display: "grid",
-        gap: "space_4",
         gridTemplateColumns: {
             mobile: "1x",
             tablet: "2x",
         },
     }),
+    {
+        columnGap: vars.spacing.space_2,
+    },
 ]);
