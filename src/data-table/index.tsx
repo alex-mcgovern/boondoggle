9b1916a82c1bfd973f60a669bff5a5d3cde3c97a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-01-08 13:13:56 +0000

    fix(data-table): add missing name prop to row actions button

--- src/data-table/index.tsx
+++ src/data-table/index.tsx
@@ -56,6 +56,7 @@ export function TableRowMenuButton<TActionId extends string>(
         <MenuButton {...props}>
             <Button
                 appearance="secondary"
+                name="row_actions"
                 size="square_sm"
             >
                 <Icon icon={faEllipsis} />
