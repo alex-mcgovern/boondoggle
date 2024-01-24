b7fbd187af03248cb73d3ac62028c2ae9d098e7c
lordbyronknox <98019344+lordbyronknox@users.noreply.github.com>
2024-01-24 11:16:33 +0200

    Component migration   tooltip (#38)
    * v2 Tooltip init index
    
    * tooltip, wip
    
    * Tooltip: V2
    
    * Tidy: Remove unused css
    
    * chore: UI
    
    * chore: relocate tooltip
    
    * 1.0.0-89
    
    ---------
    
    Co-authored-by: alex-mcgovern <alex_mcgovernsmith@icloud.com>

---
+++ src/tooltip/index.tsx
@@ -0,0 +1,63 @@
+import type {
+    TooltipProps as RACTooltipProps,
+    TooltipTriggerComponentProps as RACTooltipTriggerComponentProps,
+} from "react-aria-components";
+
+import clsx from "clsx";
+import {
+    OverlayArrow as RACOverlayArrow,
+    Tooltip as RACTooltip,
+    TooltipTrigger as RACTooltipTrigger,
+} from "react-aria-components";
+
+import { overlayArrowCSS, overlayArrowSvgCSS, tooltipCSS } from "./styles.css";
+
+export type TooltipProps = RACTooltipTriggerComponentProps & {
+    placement?: RACTooltipProps["placement"];
+    tooltipContent: React.ReactNode;
+};
+
+export const Tooltip = ({ delay = 0, ...props }: TooltipProps) => {
+    return (
+        <RACTooltipTrigger
+            {...props}
+            delay={delay}
+        >
+            {props.children}
+
+            <RACTooltip
+                {...props}
+                className={({ isEntering, isExiting, placement }) =>
+                    clsx(
+                        tooltipCSS({
+                            isEntering,
+                            isExiting,
+                            placement,
+                        }),
+                    )
+                }
+                offset={10}
+                placement={props.placement}
+            >
+                <RACOverlayArrow className={clsx(overlayArrowCSS)}>
+                    {(renderProps) => {
+                        return (
+                            <svg
+                                className={overlayArrowSvgCSS({
+                                    ...renderProps,
+                                })}
+                                height={12}
+                                viewBox="0 0 8 8"
+                                width={12}
+                            >
+                                <path d="M2 0 L4 5 L6 0" />
+                            </svg>
+                        );
+                    }}
+                </RACOverlayArrow>
+
+                {props.tooltipContent}
+            </RACTooltip>
+        </RACTooltipTrigger>
+    );
+};
