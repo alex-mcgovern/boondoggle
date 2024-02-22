4825aaad0b3eafc1c24453d1eff7794b37fc7250
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-02-22 10:25:05 +0000

    fix(skeleton): add transition

--- src/skeleton/styles.css.ts
+++ src/skeleton/styles.css.ts
@@ -1,20 +1,24 @@
 import { keyframes, style } from "@vanilla-extract/css";
 
+import { css } from "../css/index.css";
 import { vars } from "../index.css";
 
 const animationLoading = keyframes({
     to: { backgroundPositionX: "-20%" },
 });
 
-export const loadingStyles = style({
-    animation: `2s ${animationLoading} ease-in-out infinite`,
-    background: `linear-gradient(
+export const loadingStyles = style([
+    css({ transition: "short" }),
+    {
+        animation: `2s ${animationLoading} ease-in-out infinite`,
+        background: `linear-gradient(
       100deg,
       rgba(255, 255, 255, 0) 40%,
       rgba(255, 255, 255, .5) 50%,
       rgba(255, 255, 255, 0) 60%
     ) ${vars.color.tint_active}`,
-    backgroundColor: vars.color.tint_hover,
-    backgroundPositionX: "180%",
-    backgroundSize: "200% 100%",
-});
+        backgroundColor: vars.color.tint_hover,
+        backgroundPositionX: "180%",
+        backgroundSize: "200% 100%",
+    },
+]);
