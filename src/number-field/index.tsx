2e66b8de8d5a13cb8d2b0215ec2b84e35cfd39e6
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-05-09 10:53:11 +0100

    patch Intl.NumberFormatOptions

--- src/number-field/index.tsx
+++ src/number-field/index.tsx
@@ -13,6 +13,69 @@ import { FieldError } from "../field-error";
 import { Icon } from "../icon";
 import { numberFieldCSS } from "./styles.css";
 
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Intl {
+        interface NumberFormatOptions {
+            compactDisplay?: "long" | "short" | undefined;
+            currencyDisplay?: string | undefined;
+            currencySign?: string | undefined;
+            notation?:
+                | "compact"
+                | "engineering"
+                | "scientific"
+                | "standard"
+                | undefined;
+            roundingIncrement?:
+                | 1
+                | 2
+                | 5
+                | 10
+                | 20
+                | 25
+                | 50
+                | 100
+                | 200
+                | 250
+                | 500
+                | 1000
+                | 2000
+                | 2500
+                | 5000
+                | undefined;
+            roundingMode?:
+                | "ceil"
+                | "expand"
+                | "floor"
+                | "halfCeil"
+                | "halfEven"
+                | "halfExpand"
+                | "halfFloor"
+                | "halfTrunc"
+                | "trunc"
+                | undefined;
+            roundingPriority?:
+                | "auto"
+                | "lessPrecision"
+                | "morePrecision"
+                | undefined;
+            signDisplay?:
+                | "always"
+                | "auto"
+                | "exceptZero"
+                | "never"
+                | undefined;
+            trailingZeroDisplay?:
+                | "auto"
+                | "lessPrecision"
+                | "stripIfInteger"
+                | undefined;
+            unit?: string | undefined;
+            unitDisplay?: "long" | "narrow" | "short" | undefined;
+        }
+    }
+}
+
 /** -----------------------------------------------------------------------------
  * NumberFieldIncrementButton
  * ------------------------------------------------------------------------------- */
