7ebcdfddfcfff07d91ea6b5a0908d7f3ca1e28b2
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-05-13 13:08:24 +0100

    alert

---
+++ src/alert/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+import { faExclamationCircle } from "@fortawesome/pro-solid-svg-icons/faExclamationCircle";
+import clsx from "clsx";
+
+import { Icon } from "../icon";
+import "./styles.css";
+
+/**
+ * An alert is a message that is displayed to the user.
+ */
+export function Alert({
+    button_node,
+    color = "blue",
+    icon = faExclamationCircle,
+    message,
+    title,
+    ...rest
+}: {
+    button_node?: React.ReactNode;
+    color?: "amber" | "blue" | "green" | "red";
+    icon?: IconDefinition;
+    message?: string;
+    title: string;
+}) {
+    return (
+        <div
+            className={clsx("alert", color)}
+            {...rest}
+        >
+            <div className="flex align-center gap-2">
+                <Icon icon={icon} />
+                <div>
+                    {title && <div className="alert-title">{title}</div>}
+                    {message && <div className="alert-message">{message}</div>}
+                </div>
+            </div>
+
+            {button_node && <div className="ml-auto">{button_node}</div>}
+        </div>
+    );
+}
