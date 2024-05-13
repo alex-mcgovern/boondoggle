020157e9649251821d2c3cc679aa0ed77535551b
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-05-13 09:40:46 +0100

    button ref

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -5,9 +5,52 @@ import type {
 } from "react-aria-components";
 
 import clsx from "clsx";
+import { forwardRef } from "react";
 import { Button as AriaButton, Link as AriaLink } from "react-aria-components";
 
 import "./styles.css";
+
+function _Button(
+    {
+        align,
+        appearance = "primary",
+        className,
+        size,
+        ...props
+    }: AriaButtonProps & {
+        /**
+         * Alignment for the LinkButton.
+         */
+        align?: "center" | "start";
+        /**
+         * The appearance of the button.
+         */
+        appearance?: "ghost" | "primary" | "secondary";
+        /**
+         * React ref for the button element.
+         */
+        ref?: ForwardedRef<HTMLButtonElement>;
+        /**
+         * The size of the button.
+         */
+        size?: "lg" | "md" | "sm";
+        /**
+         * Whether the button is square.
+         */
+        square?: boolean;
+    },
+    ref: ForwardedRef<HTMLButtonElement>,
+) {
+    return (
+        <AriaButton
+            {...props}
+            className={clsx("btn", className, appearance, size, align, {
+                square: props.square,
+            })}
+            ref={ref}
+        />
+    );
+}
 
 /**
  * A button allows a user to perform an action, with mouse, touch, and keyboard interactions.
@@ -22,65 +65,15 @@ import "./styles.css";
  * ## Usage
  *
  * ```ts
- * import { Button, type ButtonProps } from "boondoggle/button"
+ * import { Button } from "boondoggle"
  * ```
  */
-export function Button({
-    align,
-    appearance = "primary",
-    className,
-    ref,
-    size,
-    ...props
-}: AriaButtonProps & {
-    /**
-     * Alignment for the LinkButton.
-     */
-    align?: "center" | "start";
-    /**
-     * The appearance of the button.
-     */
-    appearance?: "ghost" | "primary" | "secondary";
-    /**
-     * React ref for the button element.
-     */
-    ref?: ForwardedRef<HTMLButtonElement>;
-    /**
-     * The size of the button.
-     */
-    size?: "lg" | "md" | "sm";
-    /**
-     * Whether the button is square.
-     */
-    square?: boolean;
-}) {
-    return (
-        <AriaButton
-            {...props}
-            className={clsx("btn", className, appearance, size, align, {
-                square: props.square,
-            })}
-            ref={ref}
-        />
-    );
-}
+export const Button = forwardRef(_Button);
 
 /**
  * A link allows a user to navigate to another page or resource within a web page or application.
  *
  * [Built with React Aria Link](https://react-spectrum.adobe.com/react-aria/Link.html)
- *
- * ## Install
- *
- * ```sh
- * npm i boondoggle
- * ```
- *
- * ## Usage
- *
- * ```ts
- * import { LinkButton, type LinkButtonProps } from "boondoggle/button"
- * ```
  */
 export function LinkButton({
     align,
