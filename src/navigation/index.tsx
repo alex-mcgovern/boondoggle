6d59e06f94fc14d29728ab8aa1361737cd1541cc
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-05-13 13:43:07 +0100

    semi in jsx

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -135,7 +135,7 @@ function ButtonToggleCollapsibleNav() {
 function Container({ children }: { children: ReactNode }) {
     return (
         <Provider>
-            <div className="nav-container">{children}</div>;
+            <div className="nav-container">{children}</div>
         </Provider>
     );
 }
