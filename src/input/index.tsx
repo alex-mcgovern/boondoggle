6150afd8a4145b3de1d48578ac993c265f35cd60
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-05-13 14:45:20 +0100

    input ref

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import type { InputProps as AriaInputProps } from "react-aria-components";
 
 import clsx from "clsx";
+import { forwardRef } from "react";
 import { Input as AriaInput } from "react-aria-components";
 
 import "./styles.css";
@@ -9,8 +10,9 @@ import "./styles.css";
 /**
  * An `Input` component, for use with a `TextField` component. [Built with React Aria Input component](https://react-spectrum.adobe.com/react-aria/TextField.html#input)
  */
-export function Input(
-    props: AriaInputProps & {
+export const Input = forwardRef<
+    HTMLInputElement,
+    AriaInputProps & {
         /**
          * The icon to display on the left side of the input.
          * Designed for use with the `Icon` component from boondoggle, but can be any React node.
@@ -22,8 +24,8 @@ export function Input(
          * - `unstyled` is for use within a `Group` component, and will remove all styles from the input.
          */
         variant?: "default" | "unstyled";
-    },
-) {
+    }
+>((props) => {
     return (
         <div className="input-container">
             {props.icon && <div className="icon">{props.icon}</div>}
@@ -36,4 +38,4 @@ export function Input(
             />
         </div>
     );
-}
+});
