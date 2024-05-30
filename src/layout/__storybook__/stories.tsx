7eb7e1184180bb6acd3829b1f9e80adad9c0c0d2
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-05-30 10:15:13 +0100

    fix layout bugs

--- src/layout/__storybook__/stories.tsx
+++ src/layout/__storybook__/stories.tsx
@@ -1,12 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { faker } from "@faker-js/faker";
+import { faGear } from "@fortawesome/pro-solid-svg-icons/faGear";
 import { faPerson } from "@fortawesome/pro-solid-svg-icons/faPerson";
 import { faPlus } from "@fortawesome/pro-solid-svg-icons/faPlus";
 import { faShapes } from "@fortawesome/pro-solid-svg-icons/faShapes";
+import { faUser } from "@fortawesome/pro-solid-svg-icons/faUser";
 import { faWallet } from "@fortawesome/pro-solid-svg-icons/faWallet";
 
 import { Layout } from "..";
+import { MOCK_AVATAR } from "../../../mocks/mock_avatar";
+import { Avatar } from "../../avatar";
 import { Button } from "../../button";
 import { Icon } from "../../icon";
 import { Input } from "../../input";
@@ -16,98 +20,131 @@ import { SearchField } from "../../search-field";
 import { Table } from "../../table";
 import { Tabs } from "../../tabs";
 
+function TopNav() {
+    return (
+        <Layout.TopNav>
+            <Layout.TopNavLeft>
+                <div className="flex gap-2 align-center">
+                    <Icon icon={faShapes} />
+                    Logo
+                </div>
+            </Layout.TopNavLeft>
+            <Layout.TopNavCenter>
+                <SearchField className="w-100">
+                    <Input />
+                </SearchField>
+            </Layout.TopNavCenter>
+
+            <Layout.TopNavRight>
+                <Menu.Trigger>
+                    <Button
+                        appearance="ghost"
+                        data-testid="user-menu"
+                        size="md"
+                        square
+                    >
+                        <Avatar
+                            name="Alex McGovern"
+                            size={32}
+                            src={MOCK_AVATAR}
+                        />
+                    </Button>
+                    <Popover placement="bottom right">
+                        <Menu.MenuDecorativeSection>
+                            <Layout.UserMenuHeader
+                                avatarSrc={MOCK_AVATAR}
+                                name="Alex McGovern"
+                                organization="Acme Inc."
+                            />
+                        </Menu.MenuDecorativeSection>
+
+                        <Menu.DropdownMenu>
+                            <Menu.Item icon={<Icon icon={faUser} />}>
+                                Your profile
+                            </Menu.Item>
+
+                            <Menu.Item icon={<Icon icon={faGear} />}>
+                                Settings
+                            </Menu.Item>
+                            <Menu.Item>Log out</Menu.Item>
+                        </Menu.DropdownMenu>
+                    </Popover>
+                </Menu.Trigger>
+            </Layout.TopNavRight>
+        </Layout.TopNav>
+    );
+}
+
+function SideNav() {
+    return (
+        <Layout.SideBar>
+            <Layout.SideNavHeader>
+                <Layout.OrgDisplay
+                    image={undefined}
+                    name="Acme Inc."
+                />
+            </Layout.SideNavHeader>
+            <Layout.SideNavSection>
+                <Menu.Trigger>
+                    <Layout.Button
+                        align="center"
+                        appearance="secondary"
+                        icon={faPlus}
+                    >
+                        Create a file
+                    </Layout.Button>
+                    <Popover placement="right top">
+                        <Menu.DropdownMenu>
+                            <Menu.Section>
+                                <Menu.Item>Create new document</Menu.Item>
+                                <Menu.Item>Create new folder</Menu.Item>
+                                <Menu.Item>Create a collection</Menu.Item>
+                            </Menu.Section>
+                            <Menu.Section>
+                                <Menu.SectionHeader>Import</Menu.SectionHeader>
+                                <Menu.Item>Import an existing file</Menu.Item>
+                                <Menu.Item>Import an existing folder</Menu.Item>
+                            </Menu.Section>
+                        </Menu.DropdownMenu>
+                    </Popover>
+                </Menu.Trigger>
+            </Layout.SideNavSection>
+            <Layout.SideNavSection>
+                <Layout.Link
+                    href="https://boondoggle.design"
+                    icon={faPerson}
+                >
+                    Users
+                </Layout.Link>
+                <Layout.Link
+                    href="https://boondoggle.design"
+                    icon={faWallet}
+                >
+                    Accounts
+                </Layout.Link>
+            </Layout.SideNavSection>
+        </Layout.SideBar>
+    );
+}
+
 const meta = {
+    component: Layout.Container,
+    parameters: {
+        layout: "fullscreen",
+    },
+    title: "Layout",
+} satisfies Meta<typeof Layout.Container>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WithTabs: Story = {
     args: {
         children: (
             <>
-                <Layout.TopNav>
-                    <Layout.TopNavLeft>
-                        <div className="flex gap-2 align-center">
-                            <Icon icon={faShapes} />
-                            Logo
-                        </div>
-                    </Layout.TopNavLeft>
-                    <Layout.TopNavCenter>
-                        <SearchField className="w-100">
-                            <Input />
-                        </SearchField>
-                    </Layout.TopNavCenter>
-
-                    <Layout.TopNavRight>
-                        <Layout.Link
-                            href="https://boondoggle.design"
-                            icon={faPerson}
-                        >
-                            Users
-                        </Layout.Link>
-                        <Layout.Link
-                            href="https://boondoggle.design"
-                            icon={faWallet}
-                        >
-                            Accounts
-                        </Layout.Link>
-                    </Layout.TopNavRight>
-                </Layout.TopNav>
+                <TopNav />
                 <Layout.Body>
-                    <Layout.SideBar>
-                        <Layout.SideNavHeader>
-                            <Layout.OrgDisplay
-                                image={undefined}
-                                name="Acme Inc."
-                            />
-                        </Layout.SideNavHeader>
-                        <Layout.SideNavSection>
-                            <Menu.Trigger>
-                                <Layout.Button
-                                    align="center"
-                                    appearance="secondary"
-                                    icon={faPlus}
-                                >
-                                    Create a file
-                                </Layout.Button>
-                                <Popover placement="right top">
-                                    <Menu.DropdownMenu>
-                                        <Menu.Section>
-                                            <Menu.Item>
-                                                Create new document
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                Create new folder
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                Create a collection
-                                            </Menu.Item>
-                                        </Menu.Section>
-                                        <Menu.Section>
-                                            <Menu.SectionHeader>
-                                                Import
-                                            </Menu.SectionHeader>
-                                            <Menu.Item>
-                                                Import an existing file
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                Import an existing folder
-                                            </Menu.Item>
-                                        </Menu.Section>
-                                    </Menu.DropdownMenu>
-                                </Popover>
-                            </Menu.Trigger>
-                        </Layout.SideNavSection>
-                        <Layout.SideNavSection>
-                            <Layout.Link
-                                href="https://boondoggle.design"
-                                icon={faPerson}
-                            >
-                                Users
-                            </Layout.Link>
-                            <Layout.Link
-                                href="https://boondoggle.design"
-                                icon={faWallet}
-                            >
-                                Accounts
-                            </Layout.Link>
-                        </Layout.SideNavSection>
-                    </Layout.SideBar>
+                    <SideNav />
                     <Layout.MainContentContainer>
                         <Tabs.Container>
                             <Layout.Header backHref="/">
@@ -275,14 +312,73 @@ const meta = {
             </>
         ),
     },
-    component: Layout.Container,
-    parameters: {
-        layout: "fullscreen",
+};
+
+export const WithTable: Story = {
+    args: {
+        children: (
+            <>
+                <TopNav />
+                <Layout.Body>
+                    <SideNav />
+                    <Layout.MainContentContainer>
+                        <Layout.Header backHref="/">
+                            <h1>Table</h1>
+                        </Layout.Header>
+                        <Layout.MainContent>
+                            <Table.Root aria-label="Files">
+                                <Table.Header>
+                                    <Table.Column
+                                        allowsSorting
+                                        isRowHeader
+                                        width="2fr"
+                                    >
+                                        User
+                                    </Table.Column>
+
+                                    <Table.Column width="1fr">
+                                        Job type
+                                    </Table.Column>
+                                    <Table.Column width="1fr">
+                                        Job area
+                                    </Table.Column>
+                                </Table.Header>
+
+                                <Table.Body>
+                                    {Array.from({ length: 5 }).map(
+                                        (_, index) => {
+                                            return (
+                                                <Table.Row key={index}>
+                                                    <Table.Cell>
+                                                        {faker.person.fullName()}
+                                                    </Table.Cell>
+
+                                                    <Table.Cell>
+                                                        {faker.person.jobType()}
+                                                    </Table.Cell>
+                                                    <Table.Cell>
+                                                        {faker.person.jobArea()}
+                                                    </Table.Cell>
+                                                </Table.Row>
+                                            );
+                                        },
+                                    )}
+                                </Table.Body>
+                            </Table.Root>
+                        </Layout.MainContent>
+
+                        <Layout.Footer>
+                            <Button
+                                appearance="secondary"
+                                className="ml-auto"
+                            >
+                                Prev
+                            </Button>
+                            <Button appearance="secondary">Next</Button>
+                        </Layout.Footer>
+                    </Layout.MainContentContainer>
+                </Layout.Body>
+            </>
+        ),
     },
-    title: "Layout",
-} satisfies Meta<typeof Layout.Container>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {};
+};
