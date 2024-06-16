ff8a0897c8b0cd0eb980d9bc7a496ffd3feb4489
alex-mcgovern <58784948+alex-mcgovern@users.noreply.github.com>
2024-06-16 11:33:30 +0000

    Deploying to master from  @ aadfa39ec85c6952370fd8946447399ca647d315 🚀

---
+++ docs/assets/docs-BRzgMC1m.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-_e34SzbC.js";import{useMDXComponents as s}from"./index-BB7QKzG5.js";import{M as m,T as i,D as a,C as p}from"./index-8PSA3kyr.js";import{S as x}from"./index-xCcZr_MV.js";import{s as r,D as c}from"./stories-C4nbJjgx.js";import"./index-DVXBtNgz.js";import"./iframe-OGZiI8rf.js";import"../sb-preview/runtime.js";import"./chunk-ZHZY55TS-CJLUcfRJ.js";import"./index-BAqkEV4U.js";import"./index-7BBFQWZf.js";import"./index-CHFuvV5_.js";import"./index-DrFu-skq.js";import"./clsx-B-dksMZM.js";function e(o){const n={h2:"h2",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:r}),`
+`,t.jsx(i,{of:r}),`
+`,t.jsx(a,{of:x}),`
+`,t.jsx(n.h2,{id:"example",children:"Example"}),`
+`,t.jsx(p,{sourceState:"shown",of:c})]})}function _(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{_ as default};
