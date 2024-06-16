1301cad4464aa092f6572f9d8339df41cf3a9716
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-06-16 12:31:15 +0100

    use subcomponent style exports for switch and dialog

--- src/switch/index.tsx
+++ src/switch/index.tsx
@@ -11,33 +11,21 @@ import "./styles.css";
  * This is a separate component to allow for
  * the fact that a switch is not a semantic html form  element, and so can't be labelled in the same way.
  */
-export function SwitchLabel(props: { children: ReactNode }) {
+function SwitchLabel(props: { children: ReactNode }) {
     return <span className="switch-label">{props.children}</span>;
 }
 
 /**
  * The indicator for a switch component. This is the actual visual toggle switch UI element.
  */
-export function SwitchIndicator() {
+function SwitchIndicator() {
     return <div className="switch-indicator" />;
 }
 
 /**
  * A switch allows a user to turn a setting on or off. [Built with React Aria Switch](https://react-spectrum.adobe.com/react-aria/Switch.html)
- *
- * ## Install
- *
- * ```sh
- * npm i boondoggle
- * ```
- *
- * ## Usage
- *
- * ```ts
- * import { Switch } from "boondoggle";
- * ```
  */
-export function Switch({ ...props }: AriaSwitchProps) {
+function SwitchRoot({ ...props }: AriaSwitchProps) {
     return (
         <AriaSwitch
             className="switch"
@@ -45,3 +33,12 @@ export function Switch({ ...props }: AriaSwitchProps) {
         />
     );
 }
+
+/**
+ * A switch allows a user to turn a setting on or off. [Built with React Aria Switch](https://react-spectrum.adobe.com/react-aria/Switch.html)
+ */
+export const Switch = {
+    Indicator: SwitchIndicator,
+    Label: SwitchLabel,
+    Root: SwitchRoot,
+};
