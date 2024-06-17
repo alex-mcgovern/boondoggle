3530f2b486c737a4cedb7ffb815ddff256afdafa
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-06-17 08:30:17 +0100

    feat: styling for keyboard shortcut indicator component

--- src/keyboard-shortcut-indicator/index.tsx
+++ src/keyboard-shortcut-indicator/index.tsx
@@ -1,8 +1,17 @@
 import type { HTMLProps } from "react";
 
+import clsx from "clsx";
+
+import "./styles.css";
+
 /**
  * Signals that a keyboard shortcut is available for the user to use.
  */
 export function KeyboardShortcutIndicator(props: HTMLProps<HTMLDivElement>) {
-    return <div {...props} />;
+    return (
+        <div
+            {...props}
+            className={clsx(props.className, "keyboard-shortcut-indicator")}
+        />
+    );
 }
