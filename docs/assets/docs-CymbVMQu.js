6189ea0b034d4e11f21786d72b3d09906416fd6d
alex-mcgovern <58784948+alex-mcgovern@users.noreply.github.com>
2024-06-18 14:10:36 +0000

    Deploying to master from  @ 2906aae8c41096b79734f8a647544b1c3ac3d2a1 🚀

---
+++ docs/assets/docs-CymbVMQu.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-_e34SzbC.js";import{useMDXComponents as c}from"./index-BB7QKzG5.js";import{M as h,T as a,D as l,C as n,A as p}from"./index-B5OojOXP.js";import{s as i,S as s,D as d,R as m}from"./stories-BtdbzkCz.js";import"./index-DVXBtNgz.js";import"./iframe--o5bjJ3g.js";import"../sb-preview/runtime.js";import"./chunk-ZHZY55TS-CJLUcfRJ.js";import"./index-BAqkEV4U.js";import"./index-7BBFQWZf.js";import"./index-CHFuvV5_.js";import"./index-DrFu-skq.js";import"./utils-Bc7mYRVy.js";import"./clsx-B-dksMZM.js";import"./focusSafely-ZnAb2dR5.js";import"./platform-Dv8lzYqa.js";import"./useToggleState-9okoaj0A.js";import"./useFormReset-j9NM6Buw.js";import"./useFocusable-DnIHujbf.js";import"./useFocusRing-Dauh53Lh.js";import"./openLink-D2-6eagY.js";import"./useControlledState-DO5k2rCg.js";import"./VisuallyHidden-CPiX8Rji.js";function r(o){const e={blockquote:"blockquote",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...c(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(h,{of:i}),`
+`,t.jsx(a,{of:i}),`
+`,t.jsx(l,{of:s}),`
+`,t.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(e.p,{children:"The Switch component has a compositional API comprising of 3 components:"}),`
+`,t.jsxs(e.ul,{children:[`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"Switch"})," - the main component that wraps the ",t.jsx(e.code,{children:"SwitchInput"}),` and
+`,t.jsx(e.code,{children:"SwitchLabel"}),", and provides the state management and switch functionality."]}),`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"SwitchIndicator"})," - the visual representation of the switch state."]}),`
+`,t.jsxs(e.li,{children:[t.jsx(e.code,{children:"SwitchLabel"})," - the label for the switch."]}),`
+`]}),`
+`,t.jsx(n,{sourceState:"shown",of:d}),`
+`,t.jsxs(e.p,{children:["The positioning of the ",t.jsx(e.code,{children:"SwitchIndicator"})," relative to the ",t.jsx(e.code,{children:"SwitchLabel"}),` can be
+controlled by composing the components differently.`]}),`
+`,t.jsxs(e.blockquote,{children:[`
+`,t.jsxs(e.p,{children:["⚠️ ",t.jsx(e.strong,{children:`Typically, the correct way is to label the switch with whatever the "on"
+state is, and position that label to the right of the switch.`})]}),`
+`]}),`
+`,t.jsx(n,{sourceState:"shown",of:m}),`
+`,t.jsx(e.h2,{id:"props",children:"Props"}),`
+`,t.jsx(p,{of:s})]})}function E(o={}){const{wrapper:e}={...c(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(r,{...o})}):r(o)}export{E as default};
