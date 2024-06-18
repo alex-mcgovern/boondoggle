8b5a64c530f8e46c789ff32f28aa6c0e73c913f4
alex-mcgovern <58784948+alex-mcgovern@users.noreply.github.com>
2024-06-18 10:32:31 +0000

    Deploying to master from  @ a346caf09ce1e23126b8e2b3ef05d05f3569726f 🚀

---
+++ docs/assets/docs-BLTGCVjx.js
@@ -0,0 +1,24 @@
+import{j as o}from"./jsx-runtime-_e34SzbC.js";import{useMDXComponents as i}from"./index-BB7QKzG5.js";import{M as a,T as s,D as p,A as c}from"./index-B91T-qZp.js";import{c as m}from"./openLink-D2-6eagY.js";import"./index-DVXBtNgz.js";import"./iframe-C-eLkdfl.js";import"../sb-preview/runtime.js";import"./chunk-ZHZY55TS-CJLUcfRJ.js";import"./index-BAqkEV4U.js";import"./index-7BBFQWZf.js";import"./index-CHFuvV5_.js";import"./index-DrFu-skq.js";import"./platform-Dv8lzYqa.js";function t({children:e,navigate:r}){return o.jsx(m,{navigate:r,children:e})}try{t.displayName="RouterProvider",t.__docgenInfo={description:`RouterProvider is a re-export of the RouterProvider component from
+react-aria-components. It is used to provide client side routing capabilities
+to your application.
+[React Aria Docs](https://react-spectrum.adobe.com/react-aria/routing.html#client-side-routing)
+
+The RouterProvider component accepts a single prop: navigate. This should
+be set to a function received from your router for performing a client side
+navigation programmatically.
+
+## Install
+
+\`\`\`sh
+npm i boondoggle
+\`\`\`
+
+## Usage
+
+\`\`\`ts
+import { RouterProvider } from "boondoggle";
+\`\`\``,displayName:"RouterProvider",props:{navigate:{defaultValue:null,description:"",name:"navigate",required:!0,type:{name:"(path: string) => void"}}}}}catch{}function n(e){const r={h2:"h2",...i(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(a,{title:"Components/RouterProvider"}),`
+`,o.jsx(s,{children:"RouterProvider"}),`
+`,o.jsx(p,{of:t}),`
+`,o.jsx(r.h2,{id:"props",children:"Props"}),`
+`,o.jsx(c,{of:t})]})}function b(e={}){const{wrapper:r}={...i(),...e.components};return r?o.jsx(r,{...e,children:o.jsx(n,{...e})}):n(e)}export{b as default};
