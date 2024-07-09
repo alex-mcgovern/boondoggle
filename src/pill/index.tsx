752fae4d0ed3a047e766fdf16415f7a980ff5bee
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2024-07-09 15:50:08 +0100

    fix(`Pill`): add `slotLeft` and `slotRight` props

--- src/pill/index.tsx
+++ src/pill/index.tsx
@@ -1,3 +1,5 @@
+import type { ReactNode } from "react";
+
 import clsx from "clsx";
 import { type HTMLProps, forwardRef } from "react";
 
@@ -11,16 +13,31 @@ import "./styles.css";
 export const Pill = forwardRef<
     HTMLDivElement,
     {
+        /**
+         * The color of the pill.
+         */
         color?: Color;
+        /**
+         * Element to render on the left side of the button.
+         */
+        slotLeft?: ReactNode;
+        /**
+         * Element to render on the right side of the button.
+         */
+        slotRight?: ReactNode;
     } & HTMLProps<HTMLDivElement>
->(({ children, className, color, ...props }, ref) => {
+>(({ children, className, color, slotLeft, slotRight, ...props }, ref) => {
     return (
         <div
             {...props}
             className={clsx(className, "pill", color)}
             ref={ref}
         >
+            {slotLeft ? <div className="slot-left">{slotLeft}</div> : null}
+
             {children}
+
+            {slotRight ? <div className="slot-right">{slotRight}</div> : null}
         </div>
     );
 });
